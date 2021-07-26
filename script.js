81d3a1b24650f190fff6bdd1a74028e5916e44b1
CliffCrerar <cliff.crerar@gmail.com>
2021-07-26 14:30:56 +0200

    4 files modified, 2 added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hello!')
+console.log('Nav Links Demo in Vanilla!');
+
+// declare some tooling - these are function declarations
+const byId = id => document.getElementById(id),
+  byTag = tag => document.getElementsByTagName(tag),
+  create = el => document.createElement(el),
+  clone = el => Object.assign({}, el);
+
+// create nav elements
+const navUnorderedList = create('ul');
+const navLink = create('li');
+
+// this is a function expression - know the diffs
+function getNavLinksData() {
+  return fetch();
+}
